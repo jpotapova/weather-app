eaf6948b70570c3bcbe643aa7a90e0fb1665fa65
Jelizaveta Potapova <potapova.jelizaveta@gmail.com>
2018-10-15 19:20:11 +0300

    basic html structure

--- src/App.js
+++ src/App.js
@@ -5,7 +5,43 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello world
+        <header>
+          <a href="#">Weather app</a>
+          <ul>
+            <li><a href="">My favourites</a></li>
+            <li><a href="">My location</a></li>
+            <li><a href="">Coutries</a></li>
+            <li><button type="button">Search</button></li>
+          </ul>
+          <div><input type="text" placeholder="Search by city"/></div>
+          <div>
+            <ul>
+            <li>
+              <a href="#">Vilnius</a>
+            </li>
+            <li>
+              <a href="#">Kaunas</a>
+            </li>
+            <li>
+              <a href="#">Utena</a>
+            </li>
+            </ul>
+          </div>
+        </header>
+        <main>
+          <h1>Page title</h1>
+          <ul>
+            <li>
+              <a href="#">Lithuania</a>
+            </li>
+            <li>
+              <a href="#">United Kingdom</a>
+            </li>
+            <li>
+              <a href="#">France</a>
+            </li>
+          </ul>
+        </main>
       </div>
     );
   }

1fe87073fc128fb25dfacc8a84cb8a98e134e874
Jelizaveta Potapova <potapova.jelizaveta@gmail.com>
2018-10-16 23:32:03 +0300

    search with typeahead

--- src/Search.jsx
+++ src/Search.jsx
@@ -1,5 +1,6 @@
 import React , {Component} from 'react';
 import { Suggestions } from './Suggestions';
+const axios = require('axios');
 
 class Search extends Component {
 
@@ -8,23 +9,52 @@ class Search extends Component {
     this.listCities = this.listCities.bind(this);
 
     this.state = {
-      cities: []
+      cities: [],
+      msg: ""
     };
   }
 
   listCities(e) {
-    // fixme 
-    if (e.target.value.length > 3)
+    let text = e.target.value;
+    if (text.length) {
+      // non empty search term
+      if (text.length > 2) {
+        // long enough search term
+        // Make a request for a user with a given ID
+        fetch("http://localhost:3001/cities?name_like="+text+"&_limit=10")
+          .then(response => response.json())
+          .then(data => {
+            if (data.length > 0) {
+              // results found
+              this.setState({ cities: data, msg: "" })
+            } else {
+              // nothing found
+              this.setState({ cities: [], msg: "Nothing found" })
+            }
+          });
+      } else {
+        // search term too short
+        this.setState({
+          cities: [],
+          msg: "Type more than 2 characters to see suggestions"
+        });
+      }
+    } else {
+      // empty search term
       this.setState({
-        cities: [{"name": "Vilnius"},{"name": "Kaunas"}, {"name": "Utena"}]
+        cities: [],
+        msg: ""
       });
+    }
+
+
   }
 
   render() {
     return this.props.show ? (
       <div className="search">
-        <input type="text" placeholder="Search by city" onChange={this.listCities} />
-        <Suggestions list={this.state.cities}/>
+        <input type="text" placeholder="Start typing to search by city" onChange={this.listCities} />
+        <Suggestions list={this.state.cities} msg={this.state.msg}/>
       </div>
     ) : null;
   }
--- src/Suggestions.jsx
+++ src/Suggestions.jsx
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
 
-const Suggestions = (props) => {
-  return props.list.length ? (
-    <div className="suggestions">
-      <ul>
-        {props.list.map((city, index) => (
-          <li key={index}>
-            <a href="#">{city.name}</a>
-          </li>
-        ))}
-      </ul>
-    </div>
-  ) : null;
-};
+class Suggestions extends Component {
+  constructor(props) {
+    super(props);
+    this.formatList = this.formatList.bind(this);
+  }
+
+  formatList(list, msg) {
+    if (msg) {
+      return [{"name": msg}]
+    } else {
+      return list;
+    }
+  }
+
+  render() {
+    let list = this.formatList(this.props.list, this.props.msg);
+    return list.length ? (
+      <div className="suggestions">
+        <ul>
+          {list.map((item, index) => (
+            <li key={index}>
+              {item.id && <Link to={"/city/" + item.id} title={item.name}>{item.name}</Link>}
+              {!item.id && <span>{item.name}</span>}
+            </li>
+          ))}
+        </ul>
+      </div>
+    ) : null;
+  }
+
+
+}
 
 export { Suggestions };

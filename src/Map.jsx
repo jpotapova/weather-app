b0e5918c15005f43890b5216051035c8d808555b
Jelizaveta Potapova <potapova.jelizaveta@gmail.com>
2018-10-17 13:24:12 +0300

    maps component cleanup

--- src/Map.jsx
+++ src/Map.jsx
@@ -1,11 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Map extends Component {
   constructor(props) {
     super(props);
     this.generateMarker = this.generateMarker.bind(this);
     this.addMarker = this.addMarker.bind(this);
-    this.moveMap = this.moveMap.bind(this);
     this.initMap = this.initMap.bind(this);
     this.initMarkers = this.initMarkers.bind(this);
 
@@ -13,13 +12,17 @@ class Map extends Component {
     this.markers = [];
     this.bounds = undefined;
   }
+
   generateMarker(map, index, position) {
+
     var marker = new window.google.maps.Marker({
       position: position,
       map: map
     });
     return marker;
+
   }
+
   addMarker(markers, bounds, item, index) {
 
     let position = { lat: item.coord.lat, lng: item.coord.lon };
@@ -31,6 +34,7 @@ class Map extends Component {
     return [markers, bounds];
 
   }
+
   initMap() {
 
     this.map = new window.google.maps.Map(this.refs.map, {
@@ -42,45 +46,28 @@ class Map extends Component {
     this.bounds = new window.google.maps.LatLngBounds();
 
   }
+
   initMarkers() {
 
     // display markers and fit map to show all of them
     this.props.items.forEach((item, index) => {
       [this.markers, this.bounds] = this.addMarker(this.markers, this.bounds, item, index);
     });
-    this.moveMap(this.map, this.bounds);
+    this.map.fitBounds(this.bounds);
 
   }
   componentDidMount() {
+
     this.initMap();
-    //this.initMarkers();
+
   }
+
   componentDidUpdate() {
+
     this.initMarkers();
+
   }
-  colorMarkers(markers, index) {
-    // go through markers and assign appropriate colour
-    markers.forEach((marker, markerIndex) => {
-      if (index === marker.metadata.id) {
-        // active marker is red
-        marker.setIcon('./icon-store-red.svg');
-      } else {
-        // regular marker is black
-        marker.setIcon('./icon-store-black.svg');
-      }
-    });
-    return markers;
-  }
-  moveMap(map, bounds, index = -1, stores = []) {
-    if (index === -1) {
-      map.setZoom(16);
-      map.fitBounds(bounds);
-    } else {
-      let store = stores.find(store => store.id === index);
-      map.panTo({ lat: store.lat, lng: store.lng });
-      map.setZoom(17);
-    }
-  }
+
   render() {
     return <div ref="map" className="map" />;
   }
